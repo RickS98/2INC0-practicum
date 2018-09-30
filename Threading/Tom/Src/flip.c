8b0435c12f95ee12d00f699fd68c119be1c8eaa2
Unknown <th.buskens@gmail.com>
2018-09-30 15:56:52 +0200

    Added lots of comments

--- Threading/Tom/Src/flip.c
+++ Threading/Tom/Src/flip.c
@@ -35,19 +35,21 @@ int threadStatus[NROF_THREADS] = {THREAD_UNINITIALIZED}; //array that keeps trac
 pthread_t threadId[NROF_THREADS]; //allocation of threads
 
 struct THREADCOMMAND{
-	int numberToCheck;
-	int threadNumber;
+	int numberToCheck;//this is neccesary to know which bits to toggle
+	int threadNumber; //this is neccesary to update the status of the thread when finished
 };
 
+//This function initializes all mutexes and the semaphore
 void initialize()
 {
 	for(int i=0;i<NROF_BUFFERS;i++)
 	{
 		pthread_mutex_init ( &mutexLocks[i], NULL);//initialize all mutexes
 	}
-	sem_init(&threadCounter, 0, NROF_THREADS); //initialize semaphore
+	sem_init(&threadCounter, 0, NROF_THREADS); //initialize semaphore its initial numbers is the amount of threads that can be used
 }
 
+//This function destroys all mutexes and the semaphore
 void destroy()
 {
 	for(int i=0;i<NROF_BUFFERS;i++)
@@ -57,6 +59,7 @@ void destroy()
 	sem_destroy(&threadCounter);//cleunup semaphore
 }
 
+//This funtions sets all bits to one
 void setAllBitsOne()
 {
 	uint128_t onlyOnes = ~0;
@@ -67,6 +70,7 @@ void setAllBitsOne()
 	}
 }
 
+//This function prints the location of all found values
 void printBits()
 {
 	//print all numbers that end up true, one number per line
@@ -75,7 +79,7 @@ void printBits()
 	{
 		uint128_t temp = buffer[i];
 
-		for(int j=0;j<128 && k<NROF_PIECES; j++,k++)
+		for(int j=0;j<128 && k<=NROF_PIECES; j++,k++)
 		{
 			temp = temp >> 1;
 
@@ -88,17 +92,15 @@ void printBits()
 
 }
 
-
+//This is the thread that will be run to toggle a certain multiple of bits
 void *thread(void *arg)
 {
-	//printf("Started thread with number %d\n",numberToCheck);
+	struct THREADCOMMAND *command = (struct THREADCOMMAND*)arg; //cast arg to correct struct
 
-	struct THREADCOMMAND command = *(struct THREADCOMMAND*)arg;
-	free(arg);
+	uint128_t bitMask[NROF_BUFFERS] = {(uint128_t) 0}; //create array and sets all values to 0 to use for the masks later
 
-	uint128_t bitMask[NROF_BUFFERS] = {(uint128_t)0};
-
-	for(int i=command.numberToCheck;i<NROF_PIECES;i+=command.numberToCheck)
+	//goes to all values and updates the masks untill the masks contains each value that have to be toggled
+	for(int i=command->numberToCheck;i<=NROF_PIECES;i+=command->numberToCheck)
 	{
 		int maskIndex = i / 128;
 		int indexInMask = i % 128;
@@ -106,12 +108,11 @@ void *thread(void *arg)
 		bitMask[maskIndex] |= (((uint128_t) 1) << indexInMask);
 	}
 
-
+	//Now loop through the buffers to apply the masks
 	for(int i = 0;i<NROF_BUFFERS;i++)
 	{
 
 		//check which buffer is needed and request its mutex
-
 		int returnMutexLock = pthread_mutex_lock(&mutexLocks[i]);
 		if(returnMutexLock<0)
 		{
@@ -119,11 +120,9 @@ void *thread(void *arg)
 			exit(1);
 		}
 
-		//printf("Buffer %d: 0x%llx\n",i,buffer[i]);
-		//printf("BufferNumber: %d\nNumber: %d\nBitmask: 0x%016lx%016lx\n",i,numberToCheck,HI(bitMask), LO(bitMask));
+		buffer[i] ^= bitMask[i]; //toggle bits of mask
 
-		buffer[i] ^= bitMask[i];
-
+		//release mutex so other thread can access it again
 		int returnMutexUnlock = pthread_mutex_unlock(&mutexLocks[i]);
 		if(returnMutexUnlock<0)
 		{
@@ -131,13 +130,13 @@ void *thread(void *arg)
 			exit(1);
 		}
 
-		//printf("Result: 0x%llx\n", buffer[bufferIndex])
-
 	}
 
-	threadStatus[command.threadNumber] = THREAD_FINISHED;
+	threadStatus[command->threadNumber] = THREAD_FINISHED; //set status of current thread to finished
 
-	int returnSemPost = sem_post(&threadCounter);
+	free(arg); //clear allocated memory that was made for this thread
+
+	int returnSemPost = sem_post(&threadCounter); //increase semaphore so that main thread can clean up current thread.
 
 	if(returnSemPost<0)
 	{
@@ -145,28 +144,32 @@ void *thread(void *arg)
 		exit(1);
 	}
 
-	pthread_exit(NULL);
+	pthread_exit(NULL); //kill thread
 
 	//return NULL;
 }
 
+//function that is responsible for allocating memory of thread and starting it
 void createThread(int currentNumber,int selectedThread)
 {
-	struct THREADCOMMAND *newCommand;
-	newCommand = malloc(sizeof (struct THREADCOMMAND));
-	newCommand->numberToCheck = currentNumber;
+	struct THREADCOMMAND *newCommand; //create pointer to struct
+	newCommand = malloc(sizeof (struct THREADCOMMAND)); //allocate its memory
+	newCommand->numberToCheck = currentNumber; //and fill it with the needed values
 	newCommand->threadNumber=selectedThread;
 
-	threadStatus[selectedThread] = THREAD_BUSY;
+	threadStatus[selectedThread] = THREAD_BUSY; //sets state of thread to busy so it will be left alone
 
-	pthread_create (&threadId[selectedThread], NULL, thread, newCommand);
+	pthread_create (&threadId[selectedThread], NULL, thread, newCommand); //create the thread
 }
 
-
+//function that is responsible for going through all numbers and creating a tag for it
 void runThreads()
 {
+	//loop that goes through all numbers that have to be done
 	for(int currentNumber = 2;currentNumber<=NROF_PIECES;)
 	{
+		//wait for semaphore as a non busy wait for a thread to become finished.
+		//just goes though it when uninitialized threads are left.
 		int returnSemWait = sem_wait(&threadCounter);
 
 		if(returnSemWait<0)
@@ -174,24 +177,25 @@ void runThreads()
 			perror("Semaphore post failed");
 			exit(1);
 		}
-
+		//Goes through all threads to see which one can be used for current number
 		for(int selectedThread = 0 ;selectedThread<NROF_THREADS&&currentNumber<=NROF_PIECES;selectedThread++)
 		{
+			//if thread is not busy it can be used
 			if(threadStatus[selectedThread]!=THREAD_BUSY)
 			{
-
+				//if thread is finished it is nessesary to first join it before creating a new one.
 				if(threadStatus[selectedThread]==THREAD_FINISHED)
 				{
 					pthread_join(threadId[selectedThread], NULL);
 				}
 
 				createThread(currentNumber, selectedThread);
-
+				//increate currentNumber so next cycle the next number will be inserted in a thread
 				currentNumber++;
 			}
 		}
 	}
-
+	//wait for all thread to finish
 	for(int i = 0; i<NROF_THREADS;i++)
 	{
 		pthread_join(threadId[i], NULL);
